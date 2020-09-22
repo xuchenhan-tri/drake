9ac28b37a9277b171dc84fd7a0a00b526348c2ba
Xuchen Han <xuchen.han@tri.global>
2020-09-22 15:56:00 -0700

    Remove unused code.

--- fem/newton_solver.cc
+++ fem/newton_solver.cc
@@ -1,38 +1,6 @@
 #include "drake/fem/newton_solver.h"
 namespace drake {
 namespace fem {
-//    template <typename T>
-//    typename NewtonSolver<T>::NewtonSolverStatus NewtonSolver<T>::Solve(
-//            EigenPtr<VectorX<T>> z) const {
-//        dz_.resizeLike(*z);
-//        residual_.resizeLike(*z);
-//        UpdateAndEvalResidual(*z);
-//        for (int i = 0; i < max_iterations_; ++i) {
-//            std::unique_ptr<multibody::solvers::LinearOperator<T>> J = objective_.GetA();
-//            linear_solver_.SetUp(*J);
-//            linear_solver_.Solve(residual_, &dz_);
-//            *z += dz_;
-//            if (UpdateAndEvalResidual(*z)) {
-//                std::cout << "Newton converged in " << i + 1 << " iteration(s)."
-//                          << std::endl;
-//                return NewtonSolverStatus::Success;
-//            }
-//        }
-//        std::cout << "Newton did not converge in " << max_iterations_
-//                  << " iteration(s)." << std::endl;
-//        return NewtonSolverStatus::NoConvergence;
-//    }
-//
-//    template <typename T>
-//    bool NewtonSolver<T>::UpdateAndEvalResidual(
-//            const Eigen::Ref<const VectorX<T>>& z) const {
-//        objective_.UpdateState(z);
-//        objective_.CalcResidual(z, &residual_);
-//        // Make sure there is no NAN in the residual.
-//        DRAKE_DEMAND(residual_ == residual_);
-//        return norm(residual_) < tolerance_;
-//    }
-
 template <typename T>
 typename NewtonSolver<T>::NewtonSolverStatus NewtonSolver<T>::Solve(EigenPtr<VectorX<T>> z) const {
   residual_.resizeLike(*z);
