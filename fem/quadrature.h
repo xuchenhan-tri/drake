feadbab87583cd1e3cafc8aa79e74f7704e5d3c6
Xuchen Han <xuchen.han@tri.global>
2020-10-08 22:00:59 -0700

    Address PR feedback.

--- fem/quadrature.h
+++ fem/quadrature.h
@@ -9,16 +9,26 @@
 
 namespace drake {
 namespace fem {
-template <typename T, int D>
+template <typename T, int NaturalDimension>
 class Quadrature {
  public:
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Quadrature);
+
+  using VectorD = Eigen::Matrix<T, NaturalDimension, 1>;
+
+  template <long unsigned int NumQuads>
+  Quadrature(std::array<VectorD, NumQuads> points,
+             std::array<T, NumQuads> weights)
+      : points_(points.begin(), points.end()),
+        weights_(weights.begin(), weights.end()) {}
+
   virtual ~Quadrature() = default;
-  using VecType = Eigen::Matrix<T, D, 1>;
+
   /// The number of quadrature points for the quadrature rule.
   int num_points() const { return points_.size(); }
 
   /// The position in parent coordinate of the q-th quadrature point.
-  const VecType& get_point(int q) const {
+  const VectorD& get_point(int q) const {
     DRAKE_DEMAND(q >= 0);
     DRAKE_DEMAND(q < num_points());
     return points_[q];
@@ -31,22 +41,8 @@ class Quadrature {
     return weights_[q];
   }
 
- protected:
-  void set_points(const std::vector<VecType>& points) { points_ = points; }
-
-  void set_weights(const std::vector<T>& weights) { weights_ = weights; }
-
-  // Helper to throw a specific exception when a given dimension or order is not
-  // implemented.
-  void ThrowNotImplemented(int N) const {
-    throw std::runtime_error("Quadrature of type '" + NiceTypeName::Get(*this) +
-                             " does not have an implementation in dimension " +
-                             std::to_string(D) + " with order " +
-                             std::to_string(N) + ".");
-  }
-
  private:
-  std::vector<VecType> points_;
+  std::vector<VectorD> points_;
   std::vector<T> weights_;
 };
 
@@ -55,86 +51,77 @@ class Quadrature {
 The 2D unit triangle has vertices located at (0,0), (1,0) and (0,1). The 3D unit
 tetrahedron has vertices located at (0,0,0), (1,0,0), (0,1,0) and (0,0,1).
  @tparam N order of the quadrature rule. Must be 1, 2, or 3.
- @tparam D dimension of the unit simplex. Must be 2, or 3.
+ @tparam NaturalDimension dimension of the unit simplex. Must be 2, or 3.
 
 [Hammer, 1956] P.C. Hammer, O.P. Marlowe, and A.H. Stroud. Numerical integration
 over simplexes and cones. Math. Tables Aids Comp. 10, 130-7, 1956.
  */
-template <typename T, int N, int D>
-class SimplexGaussianQuadrature : public Quadrature<T, D> {
+template <typename T, int QuadratureOrder, int NaturalDimension>
+class SimplexGaussianQuadrature : public Quadrature<T, NaturalDimension> {
  public:
-  using VecType = typename Quadrature<T, D>::VecType;
+  using VectorD = typename Quadrature<T, NaturalDimension>::VectorD;
   DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(SimplexGaussianQuadrature);
-  SimplexGaussianQuadrature() {
-    // We only support linear, quadratic and cubic integration.
-    if constexpr (N < 1 || N > 3) {
-      this->ThrowNotImplemented(N);
-    }
-    // We only support triangles and tetrahedrons now.
-    if constexpr (D < 2 || D > 3) {
-      this->ThrowNotImplemented(N);
-    }
-    std::vector<VecType> points;
-    std::vector<T> weights;
-    if constexpr (D == 2) {
+  SimplexGaussianQuadrature()
+      : Quadrature<T, NaturalDimension>(GetPoints(), GetWeights()) {}
+
+  decltype(auto) GetPoints() {
+    static_assert(1 <= QuadratureOrder && QuadratureOrder <= 3,
+                  "Only linear, quadratic and cubic quadratures are supported");
+    if constexpr (NaturalDimension == 2) {
       // For a unit triangle, area = 0.5.
-      if constexpr (N == 1) {
+      if constexpr (QuadratureOrder == 1) {
         // quadrature point location,  weight/area
         //  (1/3, 1/3)                     1.0
-        points.resize(1, 1.0 / 3.0 * Vector2<T>::Ones(D));
-        weights.resize(1, 0.5);
-      } else if constexpr (N == 2) {
+        std::array<VectorD, 1> points = {{{1.0 / 3.0, 1.0 / 3.0}}};
+        return points;
+      } else if constexpr (QuadratureOrder == 2) {
         // quadrature point location,  weight/area
         //  (1/6, 1/6)                     1/3
         //  (2/3, 1/6)                     1/3
         //  (1/6, 2/3)                     1/3
-        points.resize(3);
-        points[0] = (Vector2<T>() << 1.0 / 6.0, 1.0 / 6.0).finished();
-        points[1] = (Vector2<T>() << 2.0 / 3.0, 1.0 / 6.0).finished();
-        points[2] = (Vector2<T>() << 1.0 / 6.0, 2.0 / 3.0).finished();
-        weights.resize(3, 1.0 / 6.0);
-      } else if constexpr (N == 3) {
+        std::array<VectorD, 3> points;
+        points[0] = {1.0 / 6.0, 1.0 / 6.0};
+        points[1] = {2.0 / 3.0, 1.0 / 6.0};
+        points[2] = {1.0 / 6.0, 2.0 / 3.0};
+        return points;
+      } else if constexpr (QuadratureOrder == 3) {
         // quadrature point location,  weight/area
         //  (1/3, 1/3)                     -9/16
         //  (3/5, 1/5)                     25/48
         //  (1/5, 3/5)                     25/48
         //  (1/5, 1/5)                     25/48
-        points.resize(4);
-        points[0] = (Vector2<T>() << 1.0 / 3.0, 1.0 / 3.0).finished();
-        points[1] = (Vector2<T>() << 0.6, 0.2).finished();
-        points[2] = (Vector2<T>() << 0.2, 0.6).finished();
-        points[3] = (Vector2<T>() << 0.2, 0.2).finished();
-        weights.resize(4);
-        weights[0] = -9.0 / 32.0;
-        for (int i = 1; i < 3; ++i) {
-          weights[i] = 25.0 / 96.0;
-        }
+        std::array<VectorD, 4> points;
+        points[0] = {1.0 / 3.0, 1.0 / 3.0};
+        points[1] = {0.6, 0.2};
+        points[2] = {0.2, 0.6};
+        points[3] = {0.2, 0.2};
+        return points;
       } else {
         DRAKE_UNREACHABLE();
       }
-    } else if constexpr (D == 3) {
+    } else if constexpr (NaturalDimension == 3) {
       // For a unit tetrahedron, area = 1/6.
-      if constexpr (N == 1) {
+      if constexpr (QuadratureOrder == 1) {
         // quadrature point location,  weight/area
         //  (1/4, 1/4, 1/4)                1.0
-        points.resize(1, 0.25 * Vector3<T>::Ones(D));
-        weights.resize(1, 1.0 / 6.0);
-      } else if constexpr (N == 2) {
+        std::array<VectorD, 1> points = {{{0.25, 0.25, 0.25}}};
+        return points;
+      } else if constexpr (QuadratureOrder == 2) {
         // quadrature point location,  weight/area
         //  (a, b, b)                      1/4
         //  (b, a, b)                      1/4
         //  (b, b, a)                      1/4
         //  (b, b, b)                      1/4
         // where a = (1+3*sqrt(1/5))/4, b = (1-1/sqrt(1/5))/4.
-        points.resize(4);
+        std::array<VectorD, 4> points;
         T a = (1.0 + 3.0 * std::sqrt(0.2)) / 4.0;
         T b = (1.0 - std::sqrt(0.2)) / 4.0;
-        points[0] = (Vector3<T>() << a, b, b).finished();
-        points[1] = (Vector3<T>() << b, a, b).finished();
-        points[2] = (Vector3<T>() << b, b, a).finished();
-        points[3] = (Vector3<T>() << b, b, b).finished();
-        weights.resize(4, 1.0 / 24.0);
-      } else if constexpr (N == 3) {
+        points[0] = {a, b, b};
+        points[1] = {b, a, b};
+        points[2] = {b, b, a};
+        points[3] = {b, b, b};
+        return points;
+      } else if constexpr (QuadratureOrder == 3) {
         // quadrature point location,  weight/area
         //  (1/4, 1/4, 1/4)               -4/5
         //  (a, b, b)                      9/20
@@ -142,28 +129,86 @@ class SimplexGaussianQuadrature : public Quadrature<T, D> {
         //  (b, b, a)                      9/20
         //  (b, b, b)                      9/20
         // where a = 1/2, b = 1/6.
-        points.resize(5);
+        std::array<VectorD, 5> points;
         T a = 0.5;
         T b = 1.0 / 6.0;
-        points[0] = (Vector3<T>() << 0.25, 0.25, 0.25).finished();
-        points[1] = (Vector3<T>() << a, b, b).finished();
-        points[2] = (Vector3<T>() << b, a, b).finished();
-        points[3] = (Vector3<T>() << b, b, a).finished();
-        points[4] = (Vector3<T>() << b, b, b).finished();
-
-        weights.resize(5);
-        weights[0] = -2.0 / 15.0;
-        for (int i = 1; i < 5; ++i) {
-          weights[i] = 3.0 / 40.0;
-        }
+        points[0] = {0.25, 0.25, 0.25};
+        points[1] = {a, b, b};
+        points[2] = {b, a, b};
+        points[3] = {b, b, a};
+        points[4] = {b, b, b};
+        return points;
       } else {
         DRAKE_UNREACHABLE();
       }
     } else {
       DRAKE_UNREACHABLE();
     }
-    this->set_points(points);
-    this->set_weights(weights);
+  }
+
+  decltype(auto) GetWeights() {
+    static_assert(1 <= QuadratureOrder && QuadratureOrder <= 3,
+                  "Only linear, quadratic and cubic quadratures are supported");
+    if constexpr (NaturalDimension == 2) {
+      // For a unit triangle, area = 0.5.
+      if constexpr (QuadratureOrder == 1) {
+        // quadrature point location,  weight/area
+        //  (1/3, 1/3)                     1.0
+        std::array<T, 1> weights = {{0.5}};
+        return weights;
+      } else if constexpr (QuadratureOrder == 2) {
+        // quadrature point location,  weight/area
+        //  (1/6, 1/6)                     1/3
+        //  (2/3, 1/6)                     1/3
+        //  (1/6, 2/3)                     1/3
+        std::array<T, 3> weights = {{1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0}};
+        return weights;
+      } else if constexpr (QuadratureOrder == 3) {
+        // quadrature point location,  weight/area
+        //  (1/3, 1/3)                     -9/16
+        //  (3/5, 1/5)                     25/48
+        //  (1/5, 3/5)                     25/48
+        //  (1/5, 1/5)                     25/48
+        std::array<T, 4> weights = {
+            {-9.0 / 32.0, 25.0 / 96.0, 25.0 / 96.0, 25.0 / 96.0}};
+        return weights;
+      } else {
+        DRAKE_UNREACHABLE();
+      }
+    } else if constexpr (NaturalDimension == 3) {
+      // For a unit tetrahedron, area = 1/6.
+      if constexpr (QuadratureOrder == 1) {
+        // quadrature point location,  weight/area
+        //  (1/4, 1/4, 1/4)                1.0
+        std::array<T, 1> weights = {{1.0 / 6.0}};
+        return weights;
+      } else if constexpr (QuadratureOrder == 2) {
+        // quadrature point location,  weight/area
+        //  (a, b, b)                      1/4
+        //  (b, a, b)                      1/4
+        //  (b, b, a)                      1/4
+        //  (b, b, b)                      1/4
+        // where a = (1+3*sqrt(1/5))/4, b = (1-1/sqrt(1/5))/4.
+        std::array<T, 4> weights = {
+            {1.0 / 24.0, 1.0 / 24.0, 1.0 / 24.0, 1.0 / 24.0}};
+        return weights;
+      } else if constexpr (QuadratureOrder == 3) {
+        // quadrature point location,  weight/area
+        //  (1/4, 1/4, 1/4)               -4/5
+        //  (a, b, b)                      9/20
+        //  (b, a, b)                      9/20
+        //  (b, b, a)                      9/20
+        //  (b, b, b)                      9/20
+        // where a = 1/2, b = 1/6.
+        std::array<T, 5> weights = {
+            {-2.0 / 15.0, 3.0 / 40.0, 3.0 / 40.0, 3.0 / 40.0, 3.0 / 40.0}};
+        return weights;
+      } else {
+        DRAKE_UNREACHABLE();
+      }
+    } else {
+      DRAKE_UNREACHABLE();
+    }
   }
 };
 }  // namespace fem
--- fem/test/simplex_gaussian_quadrature_test.cc
+++ fem/test/simplex_gaussian_quadrature_test.cc
@@ -11,19 +11,28 @@ namespace {
 
 using Eigen::Vector2d;
 using Eigen::Vector3d;
-class LinearTestFunction2D {
- public:
+struct LinearTestFunction2D {
   static constexpr double a0 = 1.0;
   static constexpr double a1 = 3.0;
   static constexpr double a2 = -2.0;
+
   // Returns the value of function
   //     y(x₀, x₁) = a₀ + a₁ * x₀+ a₂ * x₁
   // evaluated at input x.
   static double Eval(const Vector2d& x) { return a0 + a1 * x(0) + a2 * x(1); }
+
+  static double EvalAnalyticalIntegralOverUnitTriangle(){
+    // The integral of the monomial 1 and x over the unit triangle with end
+    // points at (0,0), (1,0) and (0,1) is
+    //     ∫₀¹∫₀¹⁻ˣ 1 dydx =  1/2.
+    //     ∫₀¹∫₀¹⁻ʸ x dxdy =  1/6.
+    // So the integral of f(x) = a₀ + a₁ * x₀ + a₂ * x₁ is equal to
+    //     1/2 * a₀ + 1/6 * a₁ + 1/6 * a₂.
+    return 0.5 * a0 + 1.0 / 6.0 * a1 + 1.0 / 6.0 * a2;
+  }
 };
 
-class QuadraticTestFunction2D {
- public:
+struct QuadraticTestFunction2D {
   static constexpr double a0 = 1.0;
   static constexpr double a1 = 3.0;
   static constexpr double a2 = -2.0;
@@ -37,24 +46,48 @@ class QuadraticTestFunction2D {
     return a0 + a1 * x(0) + a2 * x(1) + a3 * x(0) * x(1) + a4 * x(0) * x(0) +
            a5 * x(1) * x(1);
   }
+
+  static double EvalAnalyticalIntegralOverUnitTriangle() {
+    // The integral of the monomial 1, x, xy and x² over the unit triangle with
+    // end points at (0,0), (1,0) and (0,1) are
+    //     ∫₀¹∫₀¹⁻ˣ 1 dydx =  1/2.
+    //     ∫₀¹∫₀¹⁻ˣ x dydx =  1/6.
+    //     ∫₀¹∫₀¹⁻ʸ xy dxdy =  1/24.
+    //     ∫₀¹∫₀¹⁻ʸ x² dxdy =  1/12.
+    // So the integral of  f(x₀, x₁) = a₀ + a₁*x₀ + a₂*x₁ + a₃*x₀*x₁ + a₄*x₀² +
+    // a₅*x₁² is equal to
+    //     1/2 * a₀ + 1/6 * a₁ + 1/6 * a₂ + 1/24 * a₃ + 1/12 * a₄ + 1/12 * a₅.
+    return 0.5 * a0 + 1.0 / 6.0 * a1 + 1.0 / 6.0 * a2 + 1.0 / 24.0 * a3 +
+           1.0 / 12.0 * a4 + 1.0 / 12.0 * a5;
+  }
 };
 
-class LinearTestFunction3D {
- public:
+struct LinearTestFunction3D {
   static constexpr double a0 = 1.0;
   static constexpr double a1 = 3.0;
   static constexpr double a2 = -2.0;
   static constexpr double a3 = -4.0;
+
   // Returns the value of function
   //     y(x₀, x₁, x₂) = a₀ + a₁ * x₀+ a₂ * x₁+ a₃ * x₂
   // evaluated at input x.
   static double Eval(const Vector3d& x) {
     return a0 + a1 * x(0) + a2 * x(1) + a3 * x(2);
   }
+
+  static double EvalAnalyticalIntegralOverUnitTet(){
+    // The integral of the monomial 1 and x over the unit tetrahedron with end
+    // points at (0,0,0), (1,0,0), (0,1,0) and (0,0,1) is
+    //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ 1 dzdydx =  1/6.
+    //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ x dzdydx =  1/24.
+    // So the integral of f(x) = a₀ + a₁*x₀ + a₂*x₁ + a₃*x₂ is equal to
+    //     1/6 * a₀ + 1/24 * a₁ + 1/24 * a₂ + 1/24 * a₃.
+    return  1.0 / 6.0 * a0 + 1.0 / 24.0 * a1 +
+            1.0 / 24.0 * a2 + 1.0 / 24.0 * a3;
+  }
 };
 
-class QuadraticTestFunction3D {
- public:
+struct QuadraticTestFunction3D {
   static constexpr double a0 = 1.0;
   static constexpr double a1 = 3.0;
   static constexpr double a2 = -2.0;
@@ -65,6 +98,7 @@ class QuadraticTestFunction3D {
   static constexpr double a7 = -3.6;
   static constexpr double a8 = -2.1;
   static constexpr double a9 = 5.3;
+
   // Returns the value of function
   //     y(x₀, x₁, x₂) = a₀ + a₁ * x₀+ a₂ * x₁+ a₃ * x₂ + a₄ * x₀*x₁ + a₅ *
   //     x₀*x₂ + a₆ * x₁*x₂ + a₇*x₀²  + a₈*x₁² + a₉*x₂².
@@ -74,166 +108,91 @@ class QuadraticTestFunction3D {
            a5 * x(0) * x(2) + a6 * x(1) * x(2) + a7 * x(0) * x(0) +
            a8 * x(1) * x(1) + a9 * x(2) * x(2);
   }
+
+  static double EvalAnalyticalIntegralOverUnitTet() {
+    // The integral of the monomial 1, x, xy, and  x² over the unit tetrahedron
+    // with end points at (0,0,0), (1,0,0), (0,1,0) and (0,0,1) is
+    //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ 1 dzdydx =  1/6.
+    //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ x dzdydx =  1/24.
+    //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ xy dzdydx =  1/120.
+    //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ x² dzdydx =  1/60.
+    // So the integral of  f(x) = a₀ + a₁ * x₀+ a₂ * x₁+ a₃ * x₂  + a₄ * x₀*x₁ +
+    // a₅ * x₀*x₂ + a₆ * x₁*x₂ + a₇*x₀²  + a₈*x₁² + a₉*x₂² is equal to
+    //     1/6 * a₀ + 1/24 * a₁ + 1/24 * a₂ + 1/24 * a₃ + 1/120 * a₄ + 1/120 *
+    //     a₅ + 1/120 * a₆ + 1/60 * a₇ + 1/60 * a₈ + 1/60 * a₉
+    return 1.0 / 6.0 * a0 + 1.0 / 24.0 * a1 + 1.0 / 24.0 * a2 +
+           1.0 / 24.0 * a3 + 1.0 / 120.0 * a4 + 1.0 / 120.0 * a5 +
+           1.0 / 120.0 * a6 + 1.0 / 60.0 * a7 + 1.0 / 60.0 * a8 +
+           1.0 / 60.0 * a9;
+  }
 };
 
 class SimplexGaussianQuadratureTest : public ::testing::Test {
-  void SetUp() override {
-    linear_2d_quadrature_ =
-        std::make_unique<SimplexGaussianQuadrature<double, 1, 2>>();
-    quadratic_2d_quadrature_ =
-        std::make_unique<SimplexGaussianQuadrature<double, 2, 2>>();
-    linear_3d_quadrature_ =
-        std::make_unique<SimplexGaussianQuadrature<double, 1, 3>>();
-    quadratic_3d_quadrature_ =
-        std::make_unique<SimplexGaussianQuadrature<double, 2, 3>>();
-  }
+  void SetUp() override {}
 
  protected:
-  std::unique_ptr<SimplexGaussianQuadrature<double, 1, 2>>
-      linear_2d_quadrature_;
-  std::unique_ptr<SimplexGaussianQuadrature<double, 2, 2>>
-      quadratic_2d_quadrature_;
-  std::unique_ptr<SimplexGaussianQuadrature<double, 1, 3>>
-      linear_3d_quadrature_;
-  std::unique_ptr<SimplexGaussianQuadrature<double, 2, 3>>
-      quadratic_3d_quadrature_;
+  template <int NaturalDim>
+  double Integrate(const Quadrature<double, NaturalDim>& quadrature,
+                   double (*f)(const Eigen::Matrix<double, NaturalDim, 1>&)) {
+    double numerical_integral = 0;
+    for (int i = 0; i < quadrature.num_points(); ++i) {
+      numerical_integral +=
+          quadrature.get_weight(i) * f(quadrature.get_point(i));
+    }
+    return numerical_integral;
+  }
+
+  SimplexGaussianQuadrature<double, 1, 2> linear_2d_quadrature_;
+  SimplexGaussianQuadrature<double, 2, 2> quadratic_2d_quadrature_;
+  SimplexGaussianQuadrature<double, 1, 3> linear_3d_quadrature_;
+  SimplexGaussianQuadrature<double, 2, 3> quadratic_3d_quadrature_;
 };
 
 TEST_F(SimplexGaussianQuadratureTest, Linear2D) {
   // Linear Guassuan quadrature only needs 1 quadrature point.
-  EXPECT_EQ(linear_2d_quadrature_->num_points(), 1);
+  EXPECT_EQ(linear_2d_quadrature_.num_points(), 1);
   // Numerical integral of f = ∑ᵢ wᵢ f(xᵢ) where wᵢ is the weight of the i-th
   // quadrature point and xᵢ is the location of the i-th quadrature point.
   double numerical_integral =
-      linear_2d_quadrature_->get_weight(0) *
-      LinearTestFunction2D::Eval(linear_2d_quadrature_->get_point(0));
-  // The integral of the monomial 1 and x over the unit triangle with end points
-  // at (0,0), (1,0) and (0,1) is
-  //     ∫₀¹∫₀¹⁻ˣ 1 dydx =  1/2.
-  //     ∫₀¹∫₀¹⁻ʸ x dxdy =  1/6.
-  // So the integral of f(x) = a₀ + a₁ * x₀ + a₂ * x₁ is equal to
-  //     1/2 * a₀ + 1/6 * a₁ + 1/6 * a₂.
-  double analytic_integral = 0.5 * LinearTestFunction2D::a0 +
-                             1.0 / 6.0 * LinearTestFunction2D::a1 +
-                             1.0 / 6.0 * LinearTestFunction2D::a2;
-  EXPECT_NEAR(analytic_integral, numerical_integral,
-              std::numeric_limits<double>::epsilon());
+          Integrate(linear_2d_quadrature_, LinearTestFunction2D::Eval);
+  EXPECT_NEAR(LinearTestFunction2D::EvalAnalyticalIntegralOverUnitTriangle(),
+              numerical_integral, std::numeric_limits<double>::epsilon());
 }
 
 TEST_F(SimplexGaussianQuadratureTest, Linear3D) {
   // Linear Guassuan quadrature only needs 1 quadrature point.
-  EXPECT_EQ(linear_3d_quadrature_->num_points(), 1);
+  EXPECT_EQ(linear_3d_quadrature_.num_points(), 1);
   // Numerical integral of f = ∑ᵢ wᵢ f(xᵢ) where wᵢ is the weight of the i-th
   // quadrature point and xᵢ is the location of the i-th quadrature point.
   double numerical_integral =
-      linear_3d_quadrature_->get_weight(0) *
-      LinearTestFunction3D::Eval(linear_3d_quadrature_->get_point(0));
-  // The integral of the monomial 1 and x over the unit tetrahedron with end
-  // points at (0,0,0), (1,0,0), (0,1,0) and (0,0,1) is
-  //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ 1 dzdydx =  1/6.
-  //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ x dzdydx =  1/24.
-  // So the integral of f(x) = a₀ + a₁*x₀ + a₂*x₁ + a₃*x₂ is equal to
-  //     1/6 * a₀ + 1/24 * a₁ + 1/24 * a₂ + 1/24 * a₃.
-  double analytic_integral = 1.0 / 6.0 * LinearTestFunction3D::a0 +
-                             1.0 / 24.0 * LinearTestFunction3D::a1 +
-                             1.0 / 24.0 * LinearTestFunction3D::a2 +
-                             1.0 / 24.0 * LinearTestFunction3D::a3;
-  EXPECT_NEAR(analytic_integral, numerical_integral,
-              std::numeric_limits<double>::epsilon());
+          Integrate(linear_3d_quadrature_, LinearTestFunction3D::Eval);
+  EXPECT_NEAR(LinearTestFunction3D::EvalAnalyticalIntegralOverUnitTet(),
+          numerical_integral, std::numeric_limits<double>::epsilon());
 }
 
 TEST_F(SimplexGaussianQuadratureTest, Quadratic2D) {
   // Quadratic Guassuan quadrature needs 3 quadrature point.
-  EXPECT_EQ(quadratic_2d_quadrature_->num_points(), 3);
+  EXPECT_EQ(quadratic_2d_quadrature_.num_points(), 3);
   // Numerical integral of f = ∑ᵢ wᵢ f(xᵢ) where wᵢ is the weight of the i-th
   // quadrature point and xᵢ is the location of the i-th quadrature point.
-  double numerical_integral = 0;
-  for (int i = 0; i < quadratic_2d_quadrature_->num_points(); ++i) {
-    numerical_integral +=
-        quadratic_2d_quadrature_->get_weight(i) *
-        QuadraticTestFunction2D::Eval(quadratic_2d_quadrature_->get_point(i));
-  }
-  // The integral of the monomial 1, x, xy and x² over the unit triangle with
-  // end points at (0,0), (1,0) and (0,1) are
-  //     ∫₀¹∫₀¹⁻ˣ 1 dydx =  1/2.
-  //     ∫₀¹∫₀¹⁻ˣ x dydx =  1/6.
-  //     ∫₀¹∫₀¹⁻ʸ xy dxdy =  1/24.
-  //     ∫₀¹∫₀¹⁻ʸ x² dxdy =  1/12.
-  // So the integral of  f(x₀, x₁) = a₀ + a₁*x₀ + a₂*x₁ + a₃*x₀*x₁ + a₄*x₀² +
-  // a₅*x₁² is equal to
-  //     1/2 * a₀ + 1/6 * a₁ + 1/6 * a₂ + 1/24 * a₃ + 1/12 * a₄ + 1/12 * a₅.
-  double analytic_integral = 0.5 * QuadraticTestFunction2D::a0 +
-                             1.0 / 6.0 * QuadraticTestFunction2D::a1 +
-                             1.0 / 6.0 * QuadraticTestFunction2D::a2 +
-                             1.0 / 24.0 * QuadraticTestFunction2D::a3 +
-                             1.0 / 12.0 * QuadraticTestFunction2D::a4 +
-                             1.0 / 12.0 * QuadraticTestFunction2D::a5;
-  EXPECT_NEAR(analytic_integral, numerical_integral,
-              std::numeric_limits<double>::epsilon());
+  double numerical_integral =
+      Integrate(quadratic_2d_quadrature_, QuadraticTestFunction2D::Eval);
+  EXPECT_NEAR(QuadraticTestFunction2D::EvalAnalyticalIntegralOverUnitTriangle(),
+          numerical_integral, std::numeric_limits<double>::epsilon());
 }
 
 TEST_F(SimplexGaussianQuadratureTest, Quadratic3D) {
   // Quadratic Guassuan quadrature needs 3 quadrature point.
-  EXPECT_EQ(quadratic_3d_quadrature_->num_points(), 4);
+  EXPECT_EQ(quadratic_3d_quadrature_.num_points(), 4);
   // Numerical integral of f = ∑ᵢ wᵢ f(xᵢ) where wᵢ is the weight of the i-th
   // quadrature point and xᵢ is the location of the i-th quadrature point.
-  double numerical_integral = 0;
-  for (int i = 0; i < quadratic_3d_quadrature_->num_points(); ++i) {
-    numerical_integral +=
-        quadratic_3d_quadrature_->get_weight(i) *
-        QuadraticTestFunction3D::Eval(quadratic_3d_quadrature_->get_point(i));
-  }
-  // The integral of the monomial 1, x, xy, and  x² over the unit tetrahedron
-  // with end points at (0,0,0), (1,0,0), (0,1,0) and (0,0,1) is
-  //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ 1 dzdydx =  1/6.
-  //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ x dzdydx =  1/24.
-  //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ xy dzdydx =  1/120.
-  //     ∫₀¹∫₀¹⁻ˣ∫₀¹⁻ˣ⁻ʸ x² dzdydx =  1/60.
-  // So the integral of  f(x) = a₀ + a₁ * x₀+ a₂ * x₁+ a₃ * x₂  + a₄ * x₀*x₁ +
-  // a₅ * x₀*x₂ + a₆ * x₁*x₂ + a₇*x₀²  + a₈*x₁² + a₉*x₂² is equal to
-  //     1/6 * a₀ + 1/24 * a₁ + 1/24 * a₂ + 1/24 * a₃ + 1/120 * a₄ + 1/120 * a₅
-  //     + 1/120 * a₆ + 1/60 * a₇ + 1/60 * a₈ + 1/60 * a₉
-  double analytic_integral = 1.0 / 6.0 * QuadraticTestFunction3D::a0 +
-                             1.0 / 24.0 * QuadraticTestFunction3D::a1 +
-                             1.0 / 24.0 * QuadraticTestFunction3D::a2 +
-                             1.0 / 24.0 * QuadraticTestFunction3D::a3 +
-                             1.0 / 120.0 * QuadraticTestFunction3D::a4 +
-                             1.0 / 120.0 * QuadraticTestFunction3D::a5 +
-                             1.0 / 120.0 * QuadraticTestFunction3D::a6 +
-                             1.0 / 60.0 * QuadraticTestFunction3D::a7 +
-                             1.0 / 60.0 * QuadraticTestFunction3D::a8 +
-                             1.0 / 60.0 * QuadraticTestFunction3D::a9;
-  EXPECT_NEAR(analytic_integral, numerical_integral,
-              std::numeric_limits<double>::epsilon());
+  double numerical_integral =
+          Integrate(quadratic_3d_quadrature_, QuadraticTestFunction3D::Eval);
+  EXPECT_NEAR(QuadraticTestFunction3D::EvalAnalyticalIntegralOverUnitTet(),
+              numerical_integral, std::numeric_limits<double>::epsilon());
 }
 
 // TODO(xuchenhan-tri): Add unit tests for cubic rules.
-
-TEST_F(SimplexGaussianQuadratureTest, UnsupportedDimension) {
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 1, 1>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 1, 4>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 2, 1>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 2, 4>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 3, 1>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 3, 4>>()),
-               std::runtime_error);
-}
-
-TEST_F(SimplexGaussianQuadratureTest, UnsupportedOrder) {
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 0, 2>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 4, 2>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 0, 3>>()),
-               std::runtime_error);
-  EXPECT_THROW((std::make_unique<SimplexGaussianQuadrature<double, 4, 3>>()),
-               std::runtime_error);
-}
 }  // namespace
 }  // namespace fem
 }  // namespace drake
