5474f18bef13c162b689020aac2ca63d232e0dfd
Xuchen Han <xuchen.han@tri.global>
2022-02-01 10:08:16 -0800

    AccelerationNewmarkSchemeTest

--- multibody/fixed_fem/dev/test/acceleration_newmark_scheme_test.cc
+++ multibody/fixed_fem/dev/test/acceleration_newmark_scheme_test.cc
@@ -12,12 +12,12 @@ namespace fem {
 namespace internal {
 namespace {
 
-using fem::test::DummyElement;
+using test::DummyElement;
 
 const double kDt = 1e-3;
 const double kGamma = 0.6;
 const double kBeta = 0.3;
-const double kTolerance = 4 * std::numeric_limits<double>::epsilon();
+const double kTolerance = 8.0 * std::numeric_limits<double>::epsilon();
 
 Vector4<double> MakeQ() { return Vector4<double>(1.23, 2.34, 3.45, 4.56); }
 Vector4<double> MakeQdot() { return Vector4<double>(5.67, 6.78, 7.89, 9.10); }
@@ -38,17 +38,18 @@ GTEST_TEST(AccelerationNewmarkSchemeTest, Weights) {
  with the weights. */
 GTEST_TEST(AccelerationNewmarkSchemeTest, UpdateStateFromChangeInUnknowns) {
   AccelerationNewmarkScheme<double> scheme{kDt, kGamma, kBeta};
-  FemState<DummyElement<2>> state0(MakeQ(), MakeQdot(), MakeQddot());
-  FemState<DummyElement<2>> state(state0);
+  FemState<DummyElement> state0(MakeQ(), MakeQdot(), MakeQddot());
+  FemState<DummyElement> state(state0);
   const Vector4<double> dz(1.234, 4.567, 7.890, 0.123);
   const Vector3<double>& weights = scheme.weights();
   scheme.UpdateStateFromChangeInUnknowns(dz, &state);
+  EXPECT_TRUE(CompareMatrices(state.GetPositions() - state0.GetPositions(),
+                              weights(0) * dz, kTolerance));
+  EXPECT_TRUE(CompareMatrices(state.GetVelocities() - state0.GetVelocities(),
+                              weights(1) * dz, kTolerance));
   EXPECT_TRUE(
-      CompareMatrices(state.q() - state0.q(), weights(0) * dz, kTolerance));
-  EXPECT_TRUE(CompareMatrices(state.qdot() - state0.qdot(), weights(1) * dz,
-                              kTolerance));
-  EXPECT_TRUE(CompareMatrices(state.qddot() - state0.qddot(), weights(2) * dz,
-                              kTolerance));
+      CompareMatrices(state.GetAccelerations() - state0.GetAccelerations(),
+                      weights(2) * dz, kTolerance));
 }
 
 /* Tests that AccelerationNewmarkScheme reproduces analytical solutions with
@@ -58,24 +59,26 @@ GTEST_TEST(AccelerationNewmarkSchemeTest, AdvanceOneTimeStep) {
   const Vector4<double> q = MakeQ();
   const Vector4<double> qdot = MakeQdot();
   const Vector4<double> qddot = MakeQddot();
-  const FemState<DummyElement<2>> state_0(q, qdot, qddot);
-  FemState<DummyElement<2>> state_n(state_0);
-  FemState<DummyElement<2>> state_np1(state_0);
+  const FemState<DummyElement> state_0(q, qdot, qddot);
+  FemState<DummyElement> state_n(state_0);
+  FemState<DummyElement> state_np1(state_0);
   const int kTimeSteps = 10;
   for (int i = 0; i < kTimeSteps; ++i) {
     scheme.AdvanceOneTimeStep(state_n, qddot, &state_np1);
     state_n = state_np1;
   }
   const double total_time = kDt * kTimeSteps;
-  EXPECT_TRUE(CompareMatrices(state_n.qddot(), state_0.qddot()));
-  EXPECT_TRUE(CompareMatrices(state_n.qdot(),
-                              state_0.qdot() + total_time * state_0.qddot(),
-                              kTimeSteps * kTolerance));
   EXPECT_TRUE(
-      CompareMatrices(state_n.q(),
-                      state_0.q() + total_time * state_0.qdot() +
-                          0.5 * total_time * total_time * state_0.qddot(),
-                      kTimeSteps * kTolerance));
+      CompareMatrices(state_n.GetAccelerations(), state_0.GetAccelerations()));
+  EXPECT_TRUE(CompareMatrices(
+      state_n.GetVelocities(),
+      state_0.GetVelocities() + total_time * state_0.GetAccelerations(),
+      kTimeSteps * kTolerance));
+  EXPECT_TRUE(CompareMatrices(
+      state_n.GetPositions(),
+      state_0.GetPositions() + total_time * state_0.GetVelocities() +
+          0.5 * total_time * total_time * state_0.GetAccelerations(),
+      kTimeSteps * kTolerance));
 }
 
 /* Tests that `AccelerationNewmarkScheme` is equivalent with
@@ -84,18 +87,21 @@ GTEST_TEST(AccelerationNewmarkSchemeTest, AdvanceOneTimeStep) {
  are the same. */
 GTEST_TEST(VelocityNewmarkSchemeTest, EquivalenceWithAccelerationNewmark) {
   AccelerationNewmarkScheme<double> acceleration_scheme{kDt, kGamma, kBeta};
-  FemState<DummyElement<2>> state0(MakeQ(), MakeQdot(), MakeQddot());
-  FemState<DummyElement<2>> state_a(state0);
+  FemState<DummyElement> state0(MakeQ(), MakeQdot(), MakeQddot());
+  FemState<DummyElement> state_a(state0);
   acceleration_scheme.AdvanceOneTimeStep(state0, MakeQddot(), &state_a);
 
   VelocityNewmarkScheme<double> velocity_scheme{kDt, kGamma, kBeta};
-  FemState<DummyElement<2>> state_v(state0);
-  velocity_scheme.AdvanceOneTimeStep(state0, state_a.qdot(), &state_v);
+  FemState<DummyElement> state_v(state0);
+  velocity_scheme.AdvanceOneTimeStep(state0, state_a.GetVelocities(), &state_v);
   /* Set a larger error tolerance to accomodate the division by `dt` used in the
    VelocityNewmarkScheme. */
-  EXPECT_TRUE(CompareMatrices(state_v.qddot(), state_a.qddot(), 2.0e-12));
-  EXPECT_TRUE(CompareMatrices(state_v.qdot(), state_a.qdot(), kTolerance));
-  EXPECT_TRUE(CompareMatrices(state_v.q(), state_a.q(), kTolerance));
+  EXPECT_TRUE(CompareMatrices(state_v.GetAccelerations(),
+                              state_a.GetAccelerations(), kTolerance / kDt));
+  EXPECT_TRUE(CompareMatrices(state_v.GetVelocities(), state_a.GetVelocities(),
+                              kTolerance));
+  EXPECT_TRUE(CompareMatrices(state_v.GetPositions(), state_a.GetPositions(),
+                              kTolerance));
 }
 
 }  // namespace
