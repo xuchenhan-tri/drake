2994edcae7d53133c8861f65f251a8ab32a81210
Xuchen Han <xuchen.han@tri.global>
2024-07-30 15:05:03 -0700

    [fem] Remove quadrature template from constitutive models

--- multibody/fem/deformation_gradient_data.h
+++ multibody/fem/deformation_gradient_data.h
@@ -42,7 +42,8 @@ class DeformationGradientData<DerivedDeformationGradientData<T>> {
 
   /* Updates the data with the given deformation gradients. The deformation
    gradient dependent quantities are also updated with the given
-   deformation gradients evaluated at the current and previous time steps. */
+   deformation gradients evaluated at the current time step and the previous
+   time step. */
   void UpdateData(const Matrix3<T>& deformation_gradient,
                   const Matrix3<T>& previous_step_deformation_gradient) {
     deformation_gradient_ = deformation_gradient;
