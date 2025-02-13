a46969601d948f0e05b7f8d6b693040fc7696db5
g1n0st <g1n0st@live.com>
2025-02-12 18:30:58 -0800

    Remove some dead code

--- multibody/plant/multibody_plant.cc
+++ multibody/plant/multibody_plant.cc
@@ -2393,16 +2393,17 @@ void MultibodyPlant<T>::AddAppliedExternalSpatialForces(
       const auto body_mobod_index = body.mobod_index();
 
       // Get the pose for this body in the world frame.
-      const RigidTransform<T>& X_WB = EvalBodyPoseInWorld(context, body);
+      // const RigidTransform<T>& X_WB = EvalBodyPoseInWorld(context, body);
 
       // Get the position vector from the body origin (Bo) to the point of
       // force application (Bq), expressed in the world frame (W).
-      const Vector3<T> p_BoBq_W = X_WB.rotation() * mpm_rigid_forces.p_BoBq_B[i].template cast<T>();
+      // const Vector3<T> p_BoBq_W = X_WB.rotation() * mpm_rigid_forces.p_BoBq_B[i].template cast<T>();
 
       // Shift the spatial force from Bq to Bo.
       F_BBo_W_array[body_mobod_index] += SpatialForce<T>(
         mpm_state.external_forces_host().F_Bq_W_tau[i].template cast<T>(), 
-        mpm_state.external_forces_host().F_Bq_W_f[i].template cast<T>()).Shift(-p_BoBq_W);
+        //mpm_state.external_forces_host().F_Bq_W_f[i].template cast<T>()).Shift(-p_BoBq_W);
+        mpm_state.external_forces_host().F_Bq_W_f[i].template cast<T>());
     }
   }
 
