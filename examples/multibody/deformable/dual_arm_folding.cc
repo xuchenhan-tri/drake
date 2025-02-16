79e0a42669df9dca6abc051fc7eec509b1d392df
Chang Yu <g1n0st@live.com>
2025-02-16 12:00:45 -0800

    [fix] not have the hand not participating in the task phantom grasp

--- examples/multibody/deformable/dual_arm_folding.cc
+++ examples/multibody/deformable/dual_arm_folding.cc
@@ -127,8 +127,8 @@ class LeftGripperRotator : public systems::LeafSystem<double> {
 
 class HandPoseController : public drake::systems::LeafSystem<double> {
  public:
-  HandPoseController(const multibody::MultibodyPlant<double>& plant)
-      : plant_(plant) {
+  HandPoseController(const multibody::MultibodyPlant<double>& plant, bool is_left)
+      : plant_(plant), is_left_(is_left) {
     open_state_ = Eigen::VectorXd::Zero(4);
     open_state_(0) = -0.08;
     open_state_(1) = 0.08;
@@ -166,13 +166,13 @@ class HandPoseController : public drake::systems::LeafSystem<double> {
     
 
     // second
-    } else if (context.get_time() < 4.0) {
+    } else if (context.get_time() < 4.0 && is_left_) {
       // gripper gripping from 3.5 to 3.8, then hold until 6.0
       double t = std::max(std::min((context.get_time() - 3.5) / (0.3), 1.0), 0.0);
       Eigen::VectorXd q_and_v = std::max(1.0 - t, 0.0) * open_state_ +
                                 std::min(t, 1.0) * closed_state_2_;
       output->set_value(q_and_v);
-    } else if (context.get_time() < 6.0) {
+    } else if (context.get_time() < 6.0 && is_left_) {
       // gripper opening from 6.0 to 6.4
       double t = std::max(std::min((context.get_time() - 6.0) / (0.4), 1.0), 0.0);
       Eigen::VectorXd q_and_v = std::max(1.0 - t, 0.0) * closed_state_2_ +
@@ -180,18 +180,18 @@ class HandPoseController : public drake::systems::LeafSystem<double> {
       output->set_value(q_and_v);
     } 
     
-    else if (context.get_time() < 8.0) {
+    else if (context.get_time() < 8.0 && is_left_) {
       output->set_value(open_state_);
     } 
     
     // third
-    else if (context.get_time() < 9.0) {
+    else if (context.get_time() < 9.0 && is_left_) {
       // gripper gripping from 8.0 to 8.3, then hold until 10.0
       double t = std::max(std::min((context.get_time() - 8.0) / (0.3), 1.0), 0.0);
       Eigen::VectorXd q_and_v = std::max(1.0 - t, 0.0) * open_state_ +
                                 std::min(t, 1.0) * closed_state_3_;
       output->set_value(q_and_v);
-    } else if (context.get_time() < 13.0) {
+    } else if (context.get_time() < 13.0 && is_left_) {
       // gripper opening from 11.0 to 11.5
       double t = std::max(std::min((context.get_time() - 11.0) / (0.5), 1.0), 0.0);
       Eigen::VectorXd q_and_v = std::max(1.0 - t, 0.0) * closed_state_3_ +
@@ -206,6 +206,7 @@ class HandPoseController : public drake::systems::LeafSystem<double> {
 
  private:
   const multibody::MultibodyPlant<double>& plant_;
+  bool is_left_;
   int size_ = 4;  // 4 fingers with 4 joints
   double close_start_ = 0.05;
   double close_end_ = 0.35;
@@ -450,14 +451,14 @@ int do_main() {
   // hand controller
   auto left_hand_pose_controller =
       builder.template AddSystem<HandPoseController>(
-          plant);  // put gripper timing here
+          plant, true);  // put gripper timing here
 
   auto left_actuated_states_selector =
       builder.template AddSystem<drake::systems::MatrixGain<double>>(4);
 
   auto right_hand_pose_controller =
       builder.template AddSystem<HandPoseController>(
-          plant);  // put gripper timing here
+          plant, false);  // put gripper timing here
 
   auto right_actuated_states_selector =
       builder.template AddSystem<drake::systems::MatrixGain<double>>(4);
